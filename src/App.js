67271a70263843937c70bb6b9368b716c5cec15d
Felix Geelhaar <felix@felixgeelhaar.de>
2019-07-20 22:19:08 +0200

    fixed a type in mailto link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default () => (
   <>
     <h1>Hi there!</h1>
-    <a href="mailto=hello@felixgeelhaar.de">write me</a>
+    <a href="mailto:hello@felixgeelhaar.de">write me</a>
   </>
 )
