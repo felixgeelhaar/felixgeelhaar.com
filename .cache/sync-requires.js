b171945959ea184680a83066f96769c3700e66f8
FelixGeelhaar <felix@felixgeelhaar.de>
2020-09-08 23:36:54 +0200

    feat: remove public folder from git

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/f0978/Development/private/js/felixgeelhaar.com/.cache/dev-404-page.js"))),
   "component---src-pages-index-tsx": hot(preferDefault(require("/Users/f0978/Development/private/js/felixgeelhaar.com/src/pages/index.tsx")))
 }
 
